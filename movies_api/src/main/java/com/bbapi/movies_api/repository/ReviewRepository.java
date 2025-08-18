21da92482f5ef948c2042cd6e64981050e1b87b0
bartek <me@mywebsite.com>
2025-08-18 10:29:24 +0000

    Adding Reviewservice implementation

---
+++ movies_api/src/main/java/com/bbapi/movies_api/repository/ReviewRepository.java
@@ -0,0 +1,21 @@
+package com.bbapi.movies_api.repository;
+
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import com.bbapi.movies_api.entity.Review;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @Query("SELECT r FROM Review r WHERE r.uid = :uid;")
+    public List<Review> findUID(Long uid);
+
+    @Query("SELECT r FROM Review r WHERE r.mid = :mid;")
+    public List<Review> findMID(Long mid);
+    
+}
---
+++ movies_api/src/main/java/com/bbapi/movies_api/service/ReviewServiceImpl.java
@@ -0,0 +1,45 @@
+package com.bbapi.movies_api.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.bbapi.movies_api.entity.Review;
+import com.bbapi.movies_api.repository.ReviewRepository;
+
+public class ReviewServiceImpl implements ReviewService {
+    @Autowired
+    private ReviewRepository reviewRepo;
+
+    @Override
+    public List<Review> getReviews() {
+        // TODO Auto-generated method stub
+        return reviewRepo.findAll();
+    }
+
+    @Override
+    public List<Review> getUserReviews(Long uid) {
+        // TODO Auto-generated method stub
+        //SELECT * FROM REVIEWS where uid={uid};
+        return reviewRepo.findUID(uid);
+    }
+
+    @Override
+    public List<Review> getMovieReviews(Long mid) {
+        // TODO Auto-generated method stub
+        return reviewRepo.findMID(mid);
+    }
+
+    @Override
+    public Review postReview(Review r) {
+        // TODO Auto-generated method stub
+        return reviewRepo.save(r);
+    }
+
+    @Override
+    public void deleteReview(Long reviewID) {
+        // TODO Auto-generated method stub
+        reviewRepo.deleteById(reviewID);
+    }
+    
+}
