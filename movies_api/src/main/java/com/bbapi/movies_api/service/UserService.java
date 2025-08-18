14fb6571d5f50b380e5c0cfa49ebcc9c13d448a9
bociepka <bociepka1@gmail.com>
2025-08-18 09:32:18 +0000

    added UserService interface

---
+++ movies_api/src/main/java/com/bbapi/movies_api/service/UserService.java
@@ -0,0 +1,10 @@
+package com.bbapi.movies_api.service;
+
+import java.util.List;
+
+import com.bbapi.movies_api.entity.User;
+
+public interface UserService {
+    
+    public List<User> getAllUsers();
+}
