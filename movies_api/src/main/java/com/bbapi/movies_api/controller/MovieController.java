e3cde60644ddd8435968d7f95f648d2f3eef238a
Paulina B <paulinkaborys1@gmail.com>
2025-08-18 09:58:14 +0000

    Added Movie Controller

---
+++ movies_api/src/main/java/com/bbapi/movies_api/controller/MovieController.java
@@ -0,0 +1,53 @@
+package com.bbapi.movies_api.controller;
+
+import java.util.List;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+
+
+
+@RestController
+@RequestMapping("/movies")
+public class MovieController {
+    private final MovieService movieService;
+
+    public MovieController(MovieService movieService){
+        this.movieService=movieService;
+    }
+
+    @GetMapping
+    public List<Movie> getMovies() {
+        return movieService.getAllMovies();
+    }
+
+    @GetMapping("/{id}")
+    public String getMovieById(@RequestParam Long id) {
+        return movieService.getMovieById(id);
+    }
+
+    @PostMapping()
+    public String createMovie(@RequestBody Movie movie) {        
+        return movieService.createMovie(movie);
+    }
+    
+    @PutMapping("/{id}")
+    public String updateMovie(@PathVariable Long id, @RequestBody Movie movie) {
+        return movieService.updateMovie(id, movie);
+    }
+
+    @DeleteMapping("/{id}")
+    public String deleteMovie(@PathVariable Long id){
+        return movieService.deleteMovie(id);
+    }
+    
+    
+}
