7c27aec513eb87ad41385c1658e5702a034d18f1
bociepka <bociepka1@gmail.com>
2025-08-18 09:58:31 +0000

    added UserRepository and UserServiceImpl

---
+++ movies_api/src/main/java/com/bbapi/movies_api/service/UserServiceImpl.java
@@ -0,0 +1,39 @@
+package com.bbapi.movies_api.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+
+import com.bbapi.movies_api.entity.User;
+import com.bbapi.movies_api.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+    private UserRepository userRepository;
+
+    @Override
+    public List<User> getAllUsers() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllUsers'");
+    }
+
+    @Override
+    public User getUserById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getUserById'");
+    }
+
+    @Override
+    public User createUser(User user) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'createUser'");
+    }
+
+    @Override
+    public void deleteUser(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteUser'");
+    }
+    
+}
