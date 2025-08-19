1032048886582df2f7e760e2bf08c1f95380daad
bociepka <bociepka1@gmail.com>
2025-08-19 12:11:45 +0000

    expanded data.sql

--- movies_api/src/main/resources/data.sql
+++ movies_api/src/main/resources/data.sql
@@ -12,6 +12,12 @@ INSERT INTO review (movie_id, user_id, rating) VALUES (4, 1, 6.0);
 INSERT INTO review (movie_id, user_id, rating) VALUES (4, 2, 8.0);
 INSERT INTO review (movie_id, user_id, rating) VALUES (4, 3, 9.0);
 INSERT INTO review (movie_id, user_id, rating) VALUES (1, 1, 10.0);
+INSERT INTO review (movie_id, user_id, rating) VALUES (2, 1, 6.0);
+INSERT INTO review (movie_id, user_id, rating) VALUES (2, 2, 3.5);
+INSERT INTO review (movie_id, user_id, rating) VALUES (3, 3, 2.0);
+INSERT INTO review (movie_id, user_id, rating) VALUES (3, 1, 1.0);
 
 INSERT INTO cinephile_favourites (cinephile_id, favourites_id) VALUES (1, 1);
 INSERT INTO cinephile_favourites (cinephile_id, favourites_id) VALUES (1, 4);
+INSERT INTO cinephile_favourites (cinephile_id, favourites_id) VALUES (2, 2);
+INSERT INTO cinephile_favourites (cinephile_id, favourites_id) VALUES (2, 3);
